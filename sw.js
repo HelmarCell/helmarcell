46503ac656bfaabc96e337c34bcb11adef415c9b
Railson de Oliveira Silva <railson.de.oliveira.silva.ros@gmail.com>
2026-03-18 12:15:42 -0300

    ..

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
-// sw.js - Service Worker Básico
+// sw.js - Service Worker Simples
 self.addEventListener('install', (event) => {
-  console.log('SW: Instalado com sucesso!');
-  self.skipWaiting(); // Força o SW a se tornar ativo imediatamente
+  console.log('SW instalado');
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('SW: Ativado e pronto para rodar offline!');
-});
-
-// Este fetch vazio é o "pulo do gato" para o Chrome habilitar a instalação
 self.addEventListener('fetch', (event) => {
-  // Aqui você poderia adicionar lógica de cache, mas vazio já cumpre o requisito
+  // O Chrome exige que este evento exista para ser "instalável"
+  event.respondWith(fetch(event.request));
 });
